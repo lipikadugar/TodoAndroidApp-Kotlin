e5a973c892b24a9df751a80ce0867d2d3697d54a
Lipika Jaskaran Dugar <lipikad@Lipikas-MacBook-Pro.local>
2019-08-23 12:51:37 +0530

    Add UI to get list of all tasks

--- app/src/main/java/com/learn/todoapp/MainActivity.kt
+++ app/src/main/java/com/learn/todoapp/MainActivity.kt
@@ -6,20 +6,63 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.ListView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.learn.todoapp.db.TaskContract
 import com.learn.todoapp.db.TaskDbHelper
 
+
 class MainActivity : AppCompatActivity() {
 
-    private val mHelper = TaskDbHelper(this)
+    private var mHelper = TaskDbHelper(this)
+    private var mTaskListView: ListView? = null
+    private var mAdapter: ArrayAdapter<String>? = null
     val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.mHelper = TaskDbHelper(this)
+        this.mTaskListView = this.findViewById(R.id.list_todo)
+
+        updateUI()
+
+    }
+
+    private fun updateUI() {
+        val taskList = ArrayList<String>()
+        val db = this.mHelper.readableDatabase
+        val cursor = db.query(
+            TaskContract.TaskEntry.TABLE,
+            arrayOf(TaskContract.TaskEntry.COL_TASK_TITLE),
+            null, null, null, null, null
+        )
+        while (cursor.moveToNext()) {
+            val idx = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE)
+            taskList.add(cursor.getString(idx))
+            Log.d(TAG, "TODO: ${cursor.getString(idx)}")
+        }
+
+        if (this.mAdapter == null) {
+            mAdapter = ArrayAdapter(
+                this,
+                R.layout.item_todo,
+                R.id.task_title,
+                taskList
+            )
+            mTaskListView!!.adapter = mAdapter
+        } else {
+            mAdapter!!.clear()
+            mAdapter!!.addAll(taskList)
+            mAdapter!!.notifyDataSetChanged()
+        }
+
+        cursor.close()
+        db.close()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -49,6 +92,7 @@ class MainActivity : AppCompatActivity() {
                             SQLiteDatabase.CONFLICT_REPLACE
                         )
                         db?.close()
+                        updateUI()
                     }
                     .setNegativeButton("Cancel", null)
                     .create()
